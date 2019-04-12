bef447b6b8d00a1cc81c01362c753bba573be177
kimyonghwa <codej99@naver.com>
2019-04-12 22:59:18 +0900

    Delete unused import

--- src/main/java/com/rest/api/SpringRestApiApplication.java
+++ src/main/java/com/rest/api/SpringRestApiApplication.java
@@ -2,7 +2,6 @@ package com.rest.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class SpringRestApiApplication {
