db199aef7f382dd0bc0c564c6fc9b09bb7fabca7
kimyonghwa <codej99@nhnent.com>
2019-04-16 10:16:31 +0900

    SpringBoot2로 Rest api 만들기(5) – API 인터페이스 및 결과 데이터 구조 설계

--- src/main/java/com/rest/api/entity/User.java
+++ src/main/java/com/rest/api/entity/User.java
@@ -1,21 +1,22 @@
 package com.rest.api.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Entity
-@Getter
-@Setter
-//@Table(name = "user")
+@Builder // builder를 사용할수 있게 합니다.
+@Entity // jpa entity임을 알립니다.
+@Getter // user 필드값의 getter를 자동으로 생성합니다.
+@NoArgsConstructor // 인자없는 생성자를 자동으로 생성합니다.
+@AllArgsConstructor // 인자를 모두 갖춘 생성자를 자동으로 생성합니다.
+@Table(name = "user") // 'user' 테이블과 매핑됨을 명시
 public class User {
-    @Id // pk
+    @Id // primaryKey임을 알립니다.
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    //  pk생성전략을 DB에 위임한다는 의미입니다. mysql로 보면 pk 필드를 auto_increment로 설정해 놓은 경우로 보면 됩니다.
+    private long msrl;
+    @Column(nullable = false, unique = true, length = 30) // uid column을 명시합니다. 필수이고 유니크한 필드이며 길이는 30입니다.
+    private String uid;
+    @Column(nullable = false, length = 100) // name column을 명시합니다. 필수이고 길이는 100입니다.
     private String name;
-    private String email;
 }
--- src/main/java/com/rest/api/service/ResponseService.java
+++ src/main/java/com/rest/api/service/ResponseService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
+@Service // 해당 Class가 Service임을 명시합니다.
 public class ResponseService {
 
     // enum으로 api 요청 결과에 대한 code, message를 정의합니다.
@@ -52,11 +52,11 @@ public class ResponseService {
         return result;
     }
     // 실패 결과만 처리하는 메소드
-    public CommonResult getFailResult(int code, String msg) {
+    public CommonResult getFailResult() {
         CommonResult result = new CommonResult();
         result.setSuccess(false);
-        result.setCode(code);
-        result.setMsg(msg);
+        result.setCode(CommonResponse.FAIL.getCode());
+        result.setMsg(CommonResponse.FAIL.getMsg());
         return result;
     }
     // 결과 모델에 api 요청 성공 데이터를 세팅해주는 메소드
