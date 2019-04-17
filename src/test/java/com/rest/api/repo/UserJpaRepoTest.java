e4d5cf3a7700dc9bab46c069d7fb2ecc6b5c65b5
kimyonghwa <codej99@nhnent.com>
2019-04-17 19:24:09 +0900

    Spring Unit Test

---
+++ src/test/java/com/rest/api/repo/UserJpaRepoTest.java
@@ -0,0 +1,46 @@
+package com.rest.api.repo;
+
+import com.rest.api.entity.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Collections;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UserJpaRepoTest {
+
+    @Autowired
+    private UserJpaRepo userJpaRepo;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    public void whenFindByUid_thenReturnUser() {
+        String uid = "angrydaddy@gmail.com";
+        String name = "angrydaddy";
+        // given
+        userJpaRepo.save(User.builder()
+                .uid(uid)
+                .password(passwordEncoder.encode("1234"))
+                .name(name)
+                .roles(Collections.singletonList("ROLE_USER"))
+                .build());
+        // when
+        Optional<User> user = userJpaRepo.findByUid(uid);
+        // then
+        assertTrue(user.isPresent());
+        assertThat(user.get().getName(), is(name));
+    }
+
+}
