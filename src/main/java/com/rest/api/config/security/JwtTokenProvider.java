0226f99edce309dc360a6faa68833ff8d4504257
kimyonghwa <codej99@nhnent.com>
2019-04-22 14:48:28 +0900

    bugfix - @Value
    @Value("spring.jwt.secret") -> @Value("${spring.jwt.secret}")

--- src/main/java/com/rest/api/config/security/JwtTokenProvider.java
+++ src/main/java/com/rest/api/config/security/JwtTokenProvider.java
@@ -22,7 +22,7 @@ import java.util.List;
 @Component
 public class JwtTokenProvider { // JWT 토큰을 생성 및 검증 모듈
 
-    @Value("spring.jwt.secret")
+    @Value("${spring.jwt.secret}")
     private String secretKey;
 
     private long tokenValidMilisecond = 1000L * 60 * 60; // 1시간만 토큰 유효
